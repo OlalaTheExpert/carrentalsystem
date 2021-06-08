030e3efb5a29d7c10886b9fa920e669bc17ec29c
OlalaTheExpert <olalavictor01@gmail.com>
2021-06-08 12:16:01 +0300

    CarRental

---
+++ admin/inc/left_bar_pri.php
@@ -0,0 +1,31 @@
+<ul class="collapsibleList">
+						 <li>
+						  <label for="mylist-node1" class="quick-links"></label>
+						  <input type="checkbox" id="mylist-node1" />
+						  <ul>
+								<li></li>
+								<li><a href=""></a></li>
+								<li><a href=""></a></li>
+								<li><a href=""></a></li>
+								<li><a href=""></a></li>
+							</ul>
+						 </li>
+						 <li>
+						  <label for="mylist-node2" class="quick-links"></label>
+						  <input type="checkbox" id="mylist-node2" />
+						  <ul>
+							   <li></li>
+							   <li><a href=""></a></li>
+							  
+								<li><a href=""></a></li>
+								<li><a href=""></a></li>
+								<li><a href=""></a></li>
+									
+								
+							   
+						  </ul>
+						 </li>
+						 
+						
+						 
+					</ul>
